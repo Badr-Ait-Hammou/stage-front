64886d6c5abad8ae1ad0b8d8a21c9c2593b23960
Badr-Ait-Hammou <badr.aithammou@emsi-edu.ma>
2023-07-25 22:09:01 +0100

    Crud project

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -7,58 +7,51 @@ import { Toast } from 'primereact/toast';
 import { Button } from 'primereact/button';
 import { Toolbar } from 'primereact/toolbar';
 import { InputTextarea } from 'primereact/inputtextarea';
-//import { InputNumber } from 'primereact/inputnumber';
 import { Dialog } from 'primereact/dialog';
 import { InputText } from 'primereact/inputtext';
 import 'primeicons/primeicons.css';
-//import {DatePicker} from "@mui/lab";
 import axios from "axios";
+import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog';
 
 
 export default function Projects() {
 
-
+    const [selectedProject, setSelectedProject] = useState(null);
     const [project, setProjects] =  useState([]);
     const [name, setName] =  useState('');
-    //const [dateCreation, setDateCreation] =  useState('');
-    const [dateCreation, setDateCreation] = useState(new Date());
-    const [deleteProductsDialog, setDeleteProductsDialog] = useState(false);
-
     const [description, setDescription] =  useState('');
-    //const [gestionnaireid, setGestionnaireid] =  useState('');
-    const [deleteProductDialog, setDeleteProductDialog] = useState(false);
-
     const [productDialog, setProductDialog] = useState(false);
-    const [selectedProducts] = useState(null);
+    const [editproductDialog, seteditProductDialog] = useState(false);
     const [submitted, setSubmitted] = useState(false);
     const [globalFilter, setGlobalFilter] = useState(null);
     const toast = useRef(null);
     const dt = useRef(null);
-    const [updatedProjects, setUpdatedProjects] = useState([]);
 
     useEffect(() => {
         axios.get("http://localhost:8080/api/projet/all").then((response) => {
             setProjects(response.data);
-            setUpdatedProjects(response.data);
         });
     }, []);
 
+    const loadProjects=async ()=>{
+        const res=await axios.get(`http://localhost:8080/api/projet/all`);
+        setProjects(res.data);
+    }
 
+    /***********************Save **************/
 
 
     const handleSubmit = (event) => {
         event.preventDefault();
 
         if (name.trim() === '' || description.trim() === '') {
-            toast.current.show({ severity: 'warn', summary: 'Successful', detail: 'empty', life: 3000 });
-
-            // Show some error message or perform appropriate action when required fields are empty.
+            toast.current.show({ severity: 'error', summary: 'Successful', detail: 'empty', life: 3000 });
             return;
         }
 
         axios.post("http://localhost:8080/api/projet/save", {
                 name,
-                dateCreation,
+               // dateCreation,
                 description,
                 gestionnaire: {
                   id: 1,
@@ -69,26 +62,50 @@ export default function Projects() {
                 console.log("API Response:", response.data);
                 setName("");
                 setDescription("");
-                setDateCreation(new Date());
-              //  setGestionnaireid("");
                 setSubmitted(true);
-                //setProductDialog(false);
                 hideDialog();
-                setProjects([...updatedProjects, response.data]);
-
+                loadProjects();
+                showusave();
+                //  setDateCreation(new Date());
+                //  setGestionnaireid("");
+                //setProductDialog(false);
+                // setProjects([...updatedProjects, response.data]);
             })
             .catch((error) => {
                 console.error("Error while saving project:", error);
-                // Handle error appropriately, show error message, etc.
             });
     };
 
 
+    /***********************Delete **************/
 
 
-    const openNew = () => {
+    const handleDelete = (id) => {
+        const confirmDelete = () => {
+            axios.delete(`http://localhost:8080/api/projet/${id}`).then(() => {
+                setProjects(project.filter((rowData) => rowData.id !== id));
+                toast.current.show({severity:'error', summary: 'Done', detail:'Project deleted successfully', life: 3000});
+            });
+        };
+
+        confirmDialog({
+            message: 'Are you sure you want to Delete this Restaurant ?',
+            header: 'Confirmation',
+            icon: 'pi pi-exclamation-triangle',
+            acceptLabel: 'Yes',
+            rejectLabel: 'No',
+            acceptClassName: 'p-button-danger',
+            accept: confirmDelete
+        });
+    };
+
+
+
+        const openNew = () => {
         setProjects(project);
         setSubmitted(false);
+        setDescription("");
+        setName("");
         setProductDialog(true);
     };
 
@@ -96,69 +113,64 @@ export default function Projects() {
         setSubmitted(false);
         setProductDialog(false);
     };
-
-
-
-
-    const deleteProduct = () => {
-        let _products = products.filter((val) => val.id !== product.id);
-
-        setProducts(_products);
-        setDeleteProductDialog(false);
-        setProduct(emptyProduct);
-        toast.current.show({ severity: 'success', summary: 'Successful', detail: 'Product Deleted', life: 3000 });
+    const hideeditDialog = () => {
+       // setSubmitted(false);
+        seteditProductDialog(false);
     };
 
 
+    /***********************Update **************/
 
-    const editProduct = (project) => {
-        setProjects({ ...project });
-        setProductDialog(true);
+    const handleupdate = (rowData) => {
+        setSelectedProject(rowData);
+        setName(rowData.name);
+        setDescription(rowData.description);
+        //setDateCreation(new Date(rowData.dateCreation));
+        seteditProductDialog(true);
     };
 
-    const confirmDeleteProduct = (project) => {
-        setProjects(project);
-        setDeleteProductDialog(true);
+    const handleEdit = async (projectToUpdate) => {
+        try {
+            const response = await axios.put(`http://localhost:8080/api/projet/${projectToUpdate.id}`, {
+                name: name,
+                description: description,
+                //dateCreation: dateCreation,
+            });
+
+            const updatedProject = [...project];
+            const updatedProjectIndex = updatedProject.findIndex((project) => project.id === projectToUpdate.id);
+            updatedProject[updatedProjectIndex] = response.data;
+
+            hideeditDialog();
+            loadProjects();
+            showupdate()
+        } catch (error) {
+            console.error(error);
+        }
     };
 
-    const hideDeleteProductDialog = () => {
-        setDeleteProductDialog(false);
-    };
+    /***********************Toasts **************/
 
-    const hideDeleteProductsDialog = () => {
-        setDeleteProductsDialog(false);
-    };
+    const showupdate = () => {
+        toast.current.show({severity:'info', summary: 'success', detail:'item updated successfully', life: 3000});
+    }
+    const showusave = () => {
+        toast.current.show({severity:'success', summary: 'success', detail:'item added successfully', life: 3000});
+    }
 
 
+    /***********************csv **************/
 
-       const exportCSV = () => {
+
+    const exportCSV = () => {
         dt.current.exportCSV();
     };
-
-    const confirmDeleteSelected = () => {
-        setDeleteProductsDialog(true);
-    };
-    const deleteSelectedProducts = () => {
-        let _products = products.filter((val) => !selectedProducts.includes(val));
-
-        setProducts(_products);
-        setDeleteProductsDialog(false);
-        setSelectedProducts(null);
-        toast.current.show({ severity: 'success', summary: 'Successful', detail: 'Products Deleted', life: 3000 });
-    };
-
-
-
-
-
-
 
 
     const leftToolbarTemplate = () => {
         return (
             <div className="flex flex-wrap gap-2">
                 <Button   label="New" icon="pi pi-plus" severity="success" onClick={openNew} />
-                <Button  style={{marginLeft:"10px"}} label="Delete" icon="pi pi-trash" severity="danger" onClick={confirmDeleteSelected} disabled={!selectedProducts || !selectedProducts.length} />
             </div>
         );
     };
@@ -174,8 +186,8 @@ export default function Projects() {
     const actionBodyTemplate = (rowData) => {
         return (
             <React.Fragment>
-                <Button icon="pi pi-pencil" rounded outlined className="mr-2" onClick={() => editProduct(rowData)} />
-                <Button icon="pi pi-trash" rounded outlined severity="danger" onClick={() => confirmDeleteProduct(rowData)} />
+                <Button icon="pi pi-pencil" rounded outlined style={{marginRight:"4px"}} onClick={() => handleupdate(rowData)} />
+                <Button icon="pi pi-trash" rounded outlined severity="danger" onClick={() => handleDelete(rowData.id)} />
             </React.Fragment>
         );
     };
@@ -183,7 +195,6 @@ export default function Projects() {
 
     const header = (
         <div className="flex flex-wrap gap-2 align-items-center justify-content-between">
-            <h4 className="m-0">Manage Project</h4>
             <span className="p-input-icon-left">
                 <i className="pi pi-search" />
                 <InputText type="search" onInput={(e) => setGlobalFilter(e.target.value)} placeholder="Search..." />
@@ -198,21 +209,19 @@ export default function Projects() {
                      raised onClick={(e) => handleSubmit(e)}/>
         </React.Fragment>
     );
-    const deleteProductDialogFooter = (
+    const editproductDialogFooter = (
         <React.Fragment>
-            <Button label="No" icon="pi pi-times" outlined onClick={hideDeleteProductDialog} />
-            <Button label="Yes" icon="pi pi-check" severity="danger" onClick={deleteProduct} />
+            <Button label="Cancel" icon="pi pi-times" outlined onClick={hideeditDialog} />
+            <Button label="Update" severity="info"  raised onClick={() => handleEdit(selectedProject)} />
         </React.Fragment>
     );
-    const deleteProductsDialogFooter = (
-        <React.Fragment>
-            <Button label="No" icon="pi pi-times" outlined onClick={hideDeleteProductsDialog} />
-            <Button label="Yes" icon="pi pi-check" severity="danger" onClick={deleteSelectedProducts} />
-        </React.Fragment>
-    );
+
+
     return (
         <div>
             <Toast ref={toast} />
+            <ConfirmDialog />
+
             <div className="card">
                 <Toolbar className="mb-4" left={leftToolbarTemplate} right={rightToolbarTemplate}></Toolbar>
 
@@ -228,7 +237,7 @@ export default function Projects() {
                 </DataTable>
             </div>
 
-            <Dialog visible={productDialog} style={{ width: '32rem' }} breakpoints={{ '960px': '75vw', '641px': '90vw' }} header="Product Details" modal className="p-fluid" footer={productDialogFooter} onHide={hideDialog}>
+            <Dialog visible={productDialog} style={{ width: '32rem' }} breakpoints={{ '960px': '75vw', '641px': '90vw' }} header="Add Project" modal className="p-fluid" footer={productDialogFooter} onHide={hideDialog}>
                 <div className="field">
                     <label htmlFor="name" className="font-bold mb-4">
                         Name
@@ -242,41 +251,37 @@ export default function Projects() {
                     </label>
                     <InputTextarea id="description" value={description} onChange={(e) => setDescription(e.target.value)} required />
                 </div>
-
-
-                <div className="formgrid grid">
-                    <div className="field col">
-                        <label htmlFor="date" className="font-bold">
-                            Date
-                        </label>
-                        <InputText
-                            type={"datetime-local"}
-                            id="date"
-                            value={dateCreation.toISOString().slice(0, 16)}
-                            onChange={(e) => setDateCreation(new Date(e.target.value))}
-                            required
-                        />
-                    </div>
-
-                </div>
-            </Dialog>
-            <Dialog visible={deleteProductDialog} style={{ width: '32rem' }} breakpoints={{ '960px': '75vw', '641px': '90vw' }} header="Confirm" modal footer={deleteProductDialogFooter} onHide={hideDeleteProductDialog}>
-                <div className="confirmation-content">
-                    <i className="pi pi-exclamation-triangle mr-3" style={{ fontSize: '2rem' }} />
-                    {project && (
-                        <span>
-                            Are you sure you want to delete <b>{project.name}</b>?
-                        </span>
-                    )}
-                </div>
             </Dialog>
 
-            <Dialog visible={deleteProductsDialog} style={{ width: '32rem' }} breakpoints={{ '960px': '75vw', '641px': '90vw' }} header="Confirm" modal footer={deleteProductsDialogFooter} onHide={hideDeleteProductsDialog}>
-                <div className="confirmation-content">
-                    <i className="pi pi-exclamation-triangle mr-3" style={{ fontSize: '2rem' }} />
-                    {project && <span>Are you sure you want to delete the selected project?</span>}
+
+            <Dialog  visible={editproductDialog}
+                     style={{ width: '32rem' }}
+                     breakpoints={{ '960px': '75vw', '641px': '90vw' }}
+                     header="Edit Project"
+                     modal
+                     className="p-fluid"
+                     footer={editproductDialogFooter}
+                     onHide={hideeditDialog}
+            >
+                <div className="field">
+                    <label htmlFor="newname" className="font-bold mb-4">
+                        Name
+                    </label>
+                    <InputText id="newname" value={name} onChange={(e) => setName(e.target.value)} required />
+                    {submitted && !name && <small className="p-error">Name is required.</small>}
                 </div>
+                <div className="field">
+                    <label htmlFor="newdescription" className="font-bold">
+                        Description
+                    </label>
+                    <InputTextarea id="newdescription" value={description} onChange={(e) => setDescription(e.target.value)} required />
+                </div>
+
+
+
             </Dialog>
+
+
         </div>
     );
 }
