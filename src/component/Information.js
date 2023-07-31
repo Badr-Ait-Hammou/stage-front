83085f405490344c90707651b3090e40927be57e
Badr-Ait-Hammou <badr.aithammou@emsi-edu.ma>
2023-07-31 17:21:05 +0100

    info

--- src/component/Information.js
+++ src/component/Information.js
@@ -1,17 +1,58 @@
 import React from "react"
 import MainCard from "../ui-component/cards/MainCard";
-import {Typography} from "@mui/material";
-
+import { Panel } from 'primereact/panel';
+import {Grid} from "@mui/material";
 export default function Information(){
+
+
     return(
 
         <MainCard title="Information">
-            <Typography variant="body2">
-                Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
-                minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended
-                in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui officiate
-                descent molls anim id est labours.
-            </Typography>
+            <Panel header="Header" toggleable>
+                <Grid>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <strong>Header:</strong>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            "Header"
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={2} className="mt-0.5">
+                        <Grid item xs={12} sm={6}>
+                            <strong>Header:</strong>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            "Header"
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={2} className="mt-0.5">
+                        <Grid item xs={12} sm={6}>
+                            <strong>Header:</strong>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            "Header"
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={2} className="mt-0.5">
+                        <Grid item xs={12} sm={6}>
+                            <strong>Header:</strong>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            "Header"
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={2} className="mt-0.5">
+                        <Grid item xs={12} sm={6}>
+                            <strong>Header:</strong>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            "Header"
+                        </Grid>
+                    </Grid>
+
+                </Grid>
+            </Panel>
         </MainCard>
     );
 }
