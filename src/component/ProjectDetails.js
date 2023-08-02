19aa090d8dc8a675e6d14f2945d279d7c5209783
Badr-Ait-Hammou <badr.aithammou@emsi-edu.ma>
2023-08-02 17:47:17 +0100

    images condition

--- src/component/ProjectDetails.js
+++ src/component/ProjectDetails.js
@@ -11,22 +11,6 @@ export default function ProjectDetails() {
     const [project, setProject] = useState([]);
     const {id} = useParams();
 
-
-    const imageBodyTemplate = (photo) => {
-        return <img className=" image-item-small w-20 sm:w-16rem xl:w-10rem h-20 sm:h-16rem xl:h-10rem shadow-2 block xl:block mx-auto border-round" src={photo.photo} alt={photo.name}/>
-    };
-
-    const header = (
-        <div className="mt-2 mb-2">
-            <span className="text-xl text-900 font-bold">{project.name} Images</span>
-        </div>
-    );
-    const footer = `In total there are ${project.images ? project.images.length : 0} images on the ${project.name} project.`;
-
-
-
-
-
     useEffect(() => {
         axios.get(`http://localhost:8080/api/projet/${id}`).then((response) => {
             setProject(response.data);
@@ -34,18 +18,40 @@ export default function ProjectDetails() {
     }, []);
 
 
+    const imageBodyTemplate = (photo) => {
+        return <img className=" image-item-small " src={photo.photo} alt={photo.name}/>
+    };
+
+    const header = (
+        <div className="mt-2 mb-2">
+            <span className="text-xl text-900 font-bold">{project.name} Images</span>
+        </div>
+    );
+    const footer = (
+        <div >
+            <p>
+                In total there {project.images && project.images.length === 1 ? "is" : "are"}{" "}
+                {project.images ? project.images.length : 0}{" "}
+                {project.images && project.images.length === 1 ? "image" : "images"} on the{" "}
+                {project.name} project.
+            </p>
+        </div>
+    );
+
 
     return (
         <MainCard>
             <div className="card">
                 <Toolbar className="mb-2"  center={header}/>
-                <DataTable value={project.images}  footer={footer} tableStyle={{ minWidth: '30rem' }}>
-                    <Column field="id" sortable  header="ID"></Column>
-                    <Column header="Images" body={imageBodyTemplate}></Column>
-                    <Column field="name" filter header="Name" ></Column>
-                    <Column field="comment" header="Comment"></Column>
-                </DataTable>
-            </div>
+                <div style={{ borderRadius: '10px', overflow: 'hidden' }}>
+                    <DataTable value={project.images} footer={footer} tableStyle={{ minWidth: '30rem' }}>
+                        <Column field="id" sortable  header="ID"></Column>
+                        <Column header="Images" body={imageBodyTemplate}></Column>
+                        <Column field="name" sortable filter header="Name" ></Column>
+                        <Column field="comment" sortable header="Comment"></Column>
+                    </DataTable>
+                </div>
+                </div>
         </MainCard>
     );
 }
