206728587f7b2ac0494fb60bb362c4f9fff1869e
Badr-Ait-Hammou <badr.aithammou@emsi-edu.ma>
2023-08-09 11:48:59 +0100

    update / delete template

--- src/component/Template.js
+++ src/component/Template.js
@@ -10,7 +10,7 @@ import {Dialog} from 'primereact/dialog';
 import {InputText} from 'primereact/inputtext';
 import 'primeicons/primeicons.css';
 import axios from "axios";
-import {ConfirmDialog} from "primereact/confirmdialog";
+import {confirmDialog, ConfirmDialog} from "primereact/confirmdialog";
 import {Card, CardContent, Chip, Grid,} from '@mui/material';
 import {Box} from "@mui/system";
 import {FileUpload} from 'primereact/fileupload';
@@ -19,12 +19,18 @@ import 'leaflet-draw/dist/leaflet.draw.css'
 import {Dropdown} from 'primereact/dropdown';
 import {Chips} from "primereact/chips";
 import MainCard from "../ui-component/cards/MainCard";
+import {DataTable} from "primereact/datatable";
+import {Column} from "primereact/column";
+import {Link} from "react-router-dom";
+import PopularCart from "../ui-component/cards/Skeleton/PopularCard";
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
 
 
 export default function Template() {
 
 
     const [productDialog, setProductDialog] = useState(false);
+    const [resulteditDialog, setResultEditDialog] = useState(false);
     const [submitted, setSubmitted] = useState(false);
     const toast = useRef(null);
     const [name, setName] = useState('');
@@ -36,11 +42,20 @@ export default function Template() {
     const [image, setImages] = useState([]);
     const [result, setResult] = useState([]);
     const [fielvalue, setFieldvalue] = useState([]);
+    const [dataTableLoaded, setDataTableLoaded] = useState(false);
+    const dt = useRef(null);
+    const [selectedResult, setSelectedResult] = useState(null);
     const [resultId, setResultId] = useState("");
     const [chipsValue, setChipsValue] = useState([]);
     const [selectedResultId, setSelectedResultId] = useState('');
+    const [globalFilter, setGlobalFilter] = useState(null);
 
 
+
+
+    const handleDataTableLoad = () => {
+        setDataTableLoaded(true);
+    };
 
 
     useEffect(() => {
@@ -54,6 +69,8 @@ export default function Template() {
 
             const resultResponse = await axios.get('http://localhost:8080/api/result/all');
             setResult(resultResponse.data);
+            handleDataTableLoad();
+
         } catch (error) {
             console.error('Error fetching data:', error);
         }
@@ -64,6 +81,8 @@ export default function Template() {
             setFields(response.data);
         });
     }, []);
+
+
 
 
     const handleDeleteField = async (fieldId) => {
@@ -148,6 +167,32 @@ export default function Template() {
         });
     };
 
+    const handleEdit = async () => {
+        try {
+            const updatedProject = {
+                id: selectedResult.id,
+                name,
+                file,
+                description,
+
+            };
+            const response = await axios.put(`http://localhost:8080/api/result/${selectedResult.id}`, updatedProject);
+
+            const updatedProjects = result.map((result) =>
+                result.id === response.data.id ? response.data : result
+            );
+
+            setResult(updatedProjects);
+            hideeditDialog();
+            loadResult();
+            fetchData();
+            showuedit();
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+
 
     const handleSave = (event) => {
         event.preventDefault();
@@ -189,7 +234,6 @@ export default function Template() {
         Promise.all(savePromises)
             .then((responses) => {
                 console.log("Saved all input values:", responses);
-               // setFields(fields.map((field) => ({...field, inputValue: ""})));
                 setFields(fields.map((field) => ({ ...field, inputValue: "", saved: true }))); // Set saved to true
 
                 fetchData();
@@ -238,6 +282,31 @@ export default function Template() {
     /******************************************** Delete *************************/
 
 
+    const handleDelete = (id) => {
+        const confirmDelete = () => {
+            axios.delete(`http://localhost:8080/api/result/${id}`)
+                .then(() => {
+                    setResult(result.filter((rowData) => rowData.id !== id));
+                    toast.current.show({severity:'success', summary: 'Done', detail:'Template deleted successfully', life: 3000});
+                })
+                .catch((error) => {
+                    console.error('Error deleting project:', error);
+                    toast.current.show({severity:'error', summary: 'Error', detail:'Template Associated with a project or Fields', life: 3000});
+                });
+        };
+
+        confirmDialog({
+            message: 'Are you sure you want to Delete this Template ?',
+            header: 'Confirmation',
+            icon: 'pi pi-exclamation-triangle',
+            acceptLabel: 'Yes',
+            rejectLabel: 'No',
+            acceptClassName: 'p-button-danger',
+            accept: confirmDelete
+        });
+    };
+
+
     /******************************************** Dialogues *************************/
 
 
@@ -255,6 +324,10 @@ export default function Template() {
         setSubmitted(false);
         setProductDialog(false);
     };
+    const hideeditDialog = () => {
+        setSubmitted(false);
+        setResultEditDialog(false);
+    };
 
 
     /***********************Update **************/
@@ -264,6 +337,10 @@ export default function Template() {
 
     const showusave = () => {
         toast.current.show({severity: 'success', summary: 'success', detail: 'item added successfully', life: 3000});
+    }
+
+    const showuedit = () => {
+        toast.current.show({severity: 'info', summary: 'Done', detail: 'item updated successfully', life: 3000});
     }
 
     const showMissing = () => {
@@ -291,6 +368,14 @@ export default function Template() {
                     raised onClick={(e) => handleSubmit(e)}/>
         </React.Fragment>
     );
+    const resulteditDialogFooter = (
+        <React.Fragment>
+            <Button label="Cancel" icon="pi pi-times" outlined onClick={hideeditDialog}/>
+            <Button label="save"
+                    severity="success"
+                    raised onClick={(e) => handleEdit(e)}/>
+        </React.Fragment>
+    );
 
     const handleInputChange = (fieldId, inputValue) => {
         const updatedFields = fields.map((field) => {
@@ -314,6 +399,42 @@ export default function Template() {
     };
 
 
+    const handleupdate = (rowData) => {
+        setSelectedResult(rowData);
+        setName(rowData.name);
+        setDescription(rowData.description);
+        setFile(rowData.file);
+        setResultEditDialog(true);
+    };
+
+    const actionBodyTemplate = (rowData) => {
+        return (
+            <React.Fragment>
+                <Button icon="pi pi-pencil" rounded outlined style={{marginRight:"4px"}} onClick={() => handleupdate(rowData)} />
+                <Button icon="pi pi-trash" rounded outlined severity="danger" onClick={() => handleDelete(rowData.id)} />
+            </React.Fragment>
+        );
+    };
+    const resultFileBodyTemplate = (rowData) => {
+        if (rowData.file && rowData.file) {
+            return (
+                <a href={rowData.file} download>
+                    <FileDownloadIcon /> Download
+                </a>
+            );
+        }
+        return null;
+    };
+
+    const header = (
+        <div className="flex flex-wrap gap-2 align-items-center justify-content-between">
+            <span className="p-input-icon-left">
+                <i className="pi pi-search" />
+                <InputText type="search" onInput={(e) => setGlobalFilter(e.target.value)} placeholder="Search..." />
+            </span>
+        </div>
+    );
+
     return (
         <>
             <Toast ref={toast}/>
@@ -321,8 +442,26 @@ export default function Template() {
             <MainCard title="Manage Templates">
                 <div className="card">
                     <Toolbar className="mb-4" center={leftToolbarTemplate}></Toolbar>
+
+                    {dataTableLoaded ? (
+                        <DataTable ref={dt} value={result}
+                                   dataKey="id"  paginator rows={10} rowsPerPageOptions={[5, 10, 25]}
+                                   paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+                                   currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products" globalFilter={globalFilter} header={header}>
+                            <Column field="id" header="ID" sortable style={{ minWidth: '7rem' }}></Column>
+                            <Column field="name" header="Name" filter filterPlaceholder="Search Name ..." sortable style={{ minWidth: '10rem' }}  body={(rowData) => (
+                                <Link className="font-bold" to={`project_details/${rowData.id}`}>{rowData.name}</Link>
+                            )}></Column>
+                            <Column field="description" header="Description" sortable style={{ minWidth: '10em' }}></Column>
+                            <Column field="file" header="file" body={resultFileBodyTemplate} sortable style={{ minWidth: '10rem' }} ></Column>
+                            <Column  header="Action" body={actionBodyTemplate} exportable={false} style={{ minWidth: '12rem' }}></Column>
+                        </DataTable>
+                    ) : (
+                        <PopularCart/>
+                    )}
+
                     <Grid container spacing={2}>
-                        <Grid item xs={12}>
+                        <Grid item xs={12} sm={6}>
                             <Box className="field mt-2 " style={{display: 'flex', justifyContent: 'center'}}>
                                 <div className="card p-fluid" style={{width: '100%'}}>
                                     <span className="p-float-label" style={{width: '100%'}}>
@@ -332,8 +471,8 @@ export default function Template() {
                                 </div>
                             </Box>
                         </Grid>
-                        <Grid item xs={12}>
-                            <Box className="field" style={{display: 'flex', justifyContent: 'center'}}>
+                        <Grid item xs={12} sm={6}>
+                            <Box className="field mt-2" style={{display: 'flex', justifyContent: 'center'}}>
 
                                 <Dropdown
                                     id="resultId"
@@ -422,6 +561,75 @@ export default function Template() {
 
             <Dialog visible={productDialog} style={{width: '40rem'}} breakpoints={{'960px': '75vw', '641px': '90vw'}}
                     header="Add Template" modal className="p-fluid" footer={productDialogFooter} onHide={hideDialog}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                        <Box className="field">
+                            <label htmlFor="name" className="font-bold">
+                                Name
+                            </label>
+                            <InputText style={{marginTop: "5px"}} id="name" value={name}
+                                       onChange={(event) => setName(event.target.value)} required autoFocus/>
+                            {submitted && !image.name && <small className="p-error">Name is required.</small>}
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <Box className="field">
+                            <label htmlFor="type" className="font-bold">
+                                Type
+                            </label>
+                            <Dropdown
+                                style={{marginTop: "5px"}}
+                                id="type"
+                                value={type}
+                                options={[
+                                    {label: 'Word', value: 'word'},
+                                    {label: 'Excel', value: 'excel'}
+                                ]}
+                                onChange={(event) => setType(event.value)}
+                                optionLabel="label"
+                                optionValue="value"
+                            />
+                        </Box>
+                    </Grid>
+
+                </Grid>
+
+
+                <Grid item xs={12}>
+                    <Box className="field mt-2">
+                        <label htmlFor="image" className="font-bold">
+                            file
+                        </label>
+                        <FileUpload
+                            className="mt-2"
+                            name="file"
+                            url={'/api/upload'}
+                            maxFileSize={100000000}
+                            emptyTemplate={<p className="m-0">Drag and drop files here to upload.</p>}
+                            chooseLabel="Select File"
+                            uploadLabel="Upload"
+                            cancelLabel="Cancel"
+                            onSelect={(e) => handlefileChange(e)}
+                        />
+
+                    </Box>
+                </Grid>
+                <Box className="field mt-2">
+                    <label htmlFor="description" className="font-bold">
+                        Description
+                    </label>
+                    <InputTextarea style={{marginTop: "5px"}} id="description" value={description}
+                                   onChange={(e) => setDescription(e.target.value)} required rows={3} cols={20}/>
+                </Box>
+
+            </Dialog>
+
+
+
+
+
+            <Dialog visible={resulteditDialog} style={{width: '40rem'}} breakpoints={{'960px': '75vw', '641px': '90vw'}}
+                    header="Add Template" modal className="p-fluid" footer={resulteditDialogFooter} onHide={hideeditDialog}>
                 <Grid container spacing={2}>
                     <Grid item xs={12} sm={6}>
                         <Box className="field">
